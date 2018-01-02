ca36b6877c249e581b033da2819ae8c2529fc219
Didac Esteban Andres <didacesteban@gmail.com>
2018-01-02 18:27:06 +0100

    initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,27 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err){
+    return console.log('Unable to connect to MongoDB server: ', err);
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').insertOne({
+  //   text:'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if(err){return console.log('Impossible to insert todo')}
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  db.collection('Users').insertOne({
+    name:'Didac',
+    age: 26,
+    location: 'Terrassa'
+  }, (err, result) => {
+    if(err){return console.log('Impossible to save the user')}
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
+
+  db.close();
+});
