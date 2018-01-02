06285eea3f44f49f848a6bcb52fd1527d9a4a97a
Didac Esteban Andres <didacesteban@gmail.com>
2018-01-02 19:23:15 +0100

    understanding ids

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if(err){
@@ -14,14 +15,14 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   //   console.log(JSON.stringify(result.ops, undefined, 2));
   // });
 
-  db.collection('Users').insertOne({
-    name:'Didac',
-    age: 26,
-    location: 'Terrassa'
-  }, (err, result) => {
-    if(err){return console.log('Impossible to save the user')}
-    console.log(JSON.stringify(result.ops, undefined, 2));
-  });
+  // db.collection('Users').insertOne({
+  //   name:'Didac',
+  //   age: 26,
+  //   location: 'Terrassa'
+  // }, (err, result) => {
+  //   if(err){return console.log('Impossible to save the user')}
+  //   console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+  // });
 
   db.close();
 });
