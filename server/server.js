5daecc6186970ae6983cbe0d1b754b4386726219
Didac Esteban Andres <didacesteban@gmail.com>
2018-01-16 20:51:57 +0100

    add post todos rout and refactor mongoose

--- server/server.js
+++ server/server.js
@@ -1,52 +1,25 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-var User = mongoose.model('User', {
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 1
-  }
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 });
-
-var newUser = new User({
-  email: 'didac@didac.com'
-});
-
-newUser.save().then((doc) => {
-  console.log(JSON.stringify(doc, undefined, 2));
-},(err) => {
-  console.log(err)
-});
-
-// var otherTodo = new Todo({
-//   text: 'Watch football match',
-//   completed: false,
-//   completedAt: 1515696896
-// });
-//
-// otherTodo.save().then((doc) => {
-//   console.log('Saved: ', doc);
-// }, (e) =>{
-//   console.log(e);
-// });
