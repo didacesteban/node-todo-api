2d113975aad50b91ec7bd58a0875f2fb8c0d0a05
Didac Esteban Andres <didacesteban@gmail.com>
2018-02-04 22:43:31 +0100

    Test DELETE

--- server/test/sever.test.js
+++ server/test/sever.test.js
@@ -94,3 +94,39 @@ describe('POST /todos', () => {
         .end(done);
     })
   });
+
+  describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+      var hexId = todos[1]._id.toHexString();
+
+      request(app)
+        .delete(`/todos/${hexId}`)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.todo._id).toBe(hexId);
+        })
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+          Todo.findById(hexId).then((todo) => {
+            expect(todo).toNotExist();
+            done();
+          }).catch((err) => done(err));
+        });
+    });
+
+    it('should return 404 if todo is not found', (done) => {
+      request(app)
+        .delete(`/todos/${new ObjectID().toHexString()}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return 404 if id is not correct', (done) => {
+      request(app)
+        .delete('/todos/123')
+        .expect(404)
+        .end(done);
+    });
+  });
